10e25c646c085f960e19e7cea7f923fe9a7b4216
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-07-09 20:56:30 +0700

    test

--- resources/views/pages/compare.blade.php
+++ resources/views/pages/compare.blade.php
@@ -6,16 +6,18 @@
         <div class="col card shadow bg-body rounded p-0">
           <div class="card-header text-white fs-5" style="background-color: #0d6efd">Preference</div>
           <div class="card-body">
-            <div class="row">
-              <div class="col-8">
-                <h5 class="card-title">Filtering</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <form action="">
+              <div class="row">
+                <div class="col-8">
+                  <h5 class="card-title">Filtering</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="col-4">
+                  <h5 class="card-title">Weighting</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
               </div>
-              <div class="col-4">
-                <h5 class="card-title">Weighting</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
