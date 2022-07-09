41e7cc18ea0115ecc39ecd91743859e166c2ee95
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-07-09 10:45:15 +0700

    test

--- app/Http/Controllers/ProcessorController.php
+++ app/Http/Controllers/ProcessorController.php
@@ -7,79 +7,90 @@ use Illuminate\Http\Request;
 
 class ProcessorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function compareProcessors()
     {
-        //
-    }
+        $processors = Processor::all();
+        $costBenefit = ['n', 'n', 'c', 'b', 'b', 'b', 'b', 'c', 'n', 'n'];
+        $cMin = [];
+        $cMax = [];
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        foreach ($processors as $processor) {
+            
+        }
     }
+    // /**
+    //  * Display a listing of the resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function index()
+    // {
+    //     //
+    // }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Processor  $processor
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Processor $processor)
-    {
-        //
-    }
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Processor  $processor
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Processor $processor)
-    {
-        //
-    }
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  \App\Models\Processor  $processor
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(Processor $processor)
+    // {
+    //     //
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Processor  $processor
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Processor $processor)
-    {
-        //
-    }
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Models\Processor  $processor
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(Processor $processor)
+    // {
+    //     //
+    // }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Processor  $processor
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Processor $processor)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\Models\Processor  $processor
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, Processor $processor)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  \App\Models\Processor  $processor
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(Processor $processor)
+    // {
+    //     //
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProcessorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/compare', [ProcessorController::class, 'compareProcessors']);
