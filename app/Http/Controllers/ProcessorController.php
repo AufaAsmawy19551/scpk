328d2e4db36588e2c277f95a61c826d608ac7ff9
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-07-09 11:12:17 +0700

    test

--- app/Http/Controllers/ProcessorController.php
+++ app/Http/Controllers/ProcessorController.php
@@ -11,12 +11,68 @@ class ProcessorController extends Controller
     {
         $processors = Processor::all();
         $costBenefit = ['n', 'n', 'c', 'b', 'b', 'b', 'b', 'c', 'n', 'n'];
-        $cMin = [];
-        $cMax = [];
+        $cMin = [1000, 1000, 1000, 1000, 1000, 1000];
+        $cMax = [0, 0, 0, 0, 0, 0];
+        $min = 0;
+        $max = 0;
 
         foreach ($processors as $processor) {
-            
+            $price = $processor->price;
+            $core = $processor->core;
+            $thread = $processor->thread;
+            $boost_clock = $processor->boost_clock;
+            $cache = $processor->cache;
+            $tdp = $processor->tdp;
+
+            if ($price > $cMax[0]) {
+                $cMax[0] = $price;
+            }else if ($price < $cMin[0]) {
+                $cMin[0] = $price;
+            }
+
+            if ($core > $cMax[1]) {
+                $cMax[1] = $core;
+            }else if ($core < $cMin[1]) {
+                $cMin[1] = $core;
+            }
+
+            if ($thread > $cMax[2]) {
+                $cMax[2] = $thread;
+            }else if ($thread < $cMin[2]) {
+                $cMin[2] = $thread;
+            }
+
+            if ($boost_clock > $cMax[3]) {
+                $cMax[3] = $boost_clock;
+            }else if ($boost_clock < $cMin[3]) {
+                $cMin[3] = $boost_clock;
+            }
+
+            if ($cache > $cMax[4]) {
+                $cMax[4] = $cache;
+            }else if ($cache < $cMin[4]) {
+                $cMin[4] = $cache;
+            }
+
+            if ($tdp > $cMax[5]) {
+                $cMax[5] = $tdp;
+            }else if ($tdp < $cMin[5]) {
+                $cMin[5] = $tdp;
+            }
+
+            // var_dump($price); 
+            // var_dump($core); 
+            // var_dump($thread); 
+            // var_dump($boost_clock); 
+            // var_dump($cache); 
+            // var_dump($tdp); 
+            // if (condition) {
+            //     # code...
+            // }
         }
+
+        var_dump($cMin); 
+        var_dump($cMax); 
     }
     // /**
     //  * Display a listing of the resource.
